198c39107c1940646adc9a0d1832fd50d1b9e170
ikun <ikun0014@qq.com>
2024-06-12 22:52:07 +0800

    re

---
+++ src/utils/musicSdk/kg/api-ikun.js
@@ -0,0 +1,20 @@
+import { httpFetch } from '../../request'
+import { requestMsg } from '../../message'
+import { headers, timeout } from '../options'
+
+const api_ikun = {
+  getMusicUrl(songInfo, type) {
+    const requestObj = httpFetch(`http://110.42.111.49:1314/url/kg/${songInfo._types[type].hash}/${type}`, {
+      method: 'get',
+      timeout,
+      headers,
+      family: 4,
+    })
+    requestObj.promise = requestObj.promise.then(({ body }) => {
+      return body.code === 0 ? Promise.resolve({ type, url: body.data }) : Promise.reject(new Error(requestMsg.fail))
+    })
+    return requestObj
+  },
+}
+
+export default api_ikun
